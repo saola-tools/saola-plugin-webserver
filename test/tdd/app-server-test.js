e1f89d6635e750809932bba92b02b4a9e21121b1
pnhung177 <pnhung177@gmail.com>
2022-12-16 09:52:42 +0700

    Add the app.server start/stop cycle testcase

---
+++ test/tdd/app-server-test.js
@@ -0,0 +1,16 @@
+'use strict';
+
+const app = require('../../server');
+
+describe('app-webserver', function() {
+  describe('start/stop app.server', function() {
+    it('app.server should be started/stopped properly', function(done) {
+      app.server.start().then(function() {
+        return app.server.stop();
+      }).then(function() {
+        done();
+      });
+      this.timeout(6000);
+    });
+  });
+});
