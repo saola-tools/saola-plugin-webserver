71095356766f98d3e8048c3ebb4daf39afe1fcb7
pnhung177 <pnhung177@gmail.com>
2022-12-17 05:38:21 +0700

    Enhance the app-server-test

--- test/tdd/app-server-test.js
+++ test/tdd/app-server-test.js
@@ -1,16 +1,34 @@
-'use strict';
+"use strict";
 
-const app = require('../../app');
+const Devebot = require("devebot");
+const LogConfig = Devebot.require("logolite").LogConfig;
+const LogTracer = Devebot.require("logolite").LogTracer;
+const envcloak = require("envcloak").instance;
 
-describe('app-webserver', function() {
-  describe('start/stop app.server', function() {
-    it('app.server should be started/stopped properly', function(done) {
+const app = require("../../app");
+
+describe("app-webserver", function() {
+  describe("start/stop app.server", function() {
+    before(function() {
+      envcloak.setup({
+        LOGOLITE_FULL_LOG_MODE: "false",
+        LOGOLITE_ALWAYS_ENABLED: "all",
+        LOGOLITE_ALWAYS_MUTED: "all"
+      });
+      LogConfig.reset();
+    });
+    //
+    after(function() {
+      LogTracer.clearInterceptors();
+      envcloak.reset();
+    });
+    //
+    it("app.server should be started/stopped properly", function(done) {
       app.server.start().then(function() {
         return app.server.stop();
       }).then(function() {
         done();
       });
-      this.timeout(6000);
     });
   });
 });
